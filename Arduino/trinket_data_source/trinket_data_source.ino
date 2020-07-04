0376db3e8c92a381ee54e79a25770ea4ae56ad4e
Mark Nyberg <bigusdeveloper@gmail.com>
2020-07-04 15:03:37 -0400

    Added initial file version of Arduino code for the data sender.

---
+++ Arduino/trinket_data_source/trinket_data_source.ino
@@ -0,0 +1,38 @@
+/* Program design notes
+ *    This protram is intended to setup a Trinket Pro 3.3V MCU for acquiring data from a sonar rangefinder and BME280 and pushing that data
+ *    via a RFM69HCW module to a like module paired with Raspberry Pi Zero W running Rasbian and Python.
+ *    
+ *    ---------------Build plan------------------
+ *    Sleep system for up to 5 minutes          TODO
+ *    Wake the system for data acq              TODO
+ *    Prep BME280 for data gathering            TODO
+ *    Acquire data from BME280                  TODO
+ *    Sleep BME280                              TODO
+ *    Prep range finder for data gathering      TODO
+ *    Acquire data on range/tide                TODO
+ *    Sleep range finder                        TODO
+ *    Collect battery charge status             TODO
+ *    Format data for transmission              TODO
+ *    Wake RFM69HCW                             TODO
+ *    Configure RFM69HCW                        TODO
+ *    Transmit data via RFM69HCW                TODO
+ *    Sleep RFM69HCW                            TODO
+ *    
+ *    
+ */
+
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
