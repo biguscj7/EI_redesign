d5c491746b5f3f2ac097aea5b11cb7389f63ab10
biguscj7 <bigusdeveloper@gmail.com>
2020-07-08 21:09:37 -0400

    Create display.py
    Adding the file for display operation. Currently only TODO, no code.

---
+++ Python/display.py
@@ -0,0 +1,18 @@
+import urequests
+import ujson
+
+#TODO: get high/low tide at Beaufort
+
+#TODO: parse hi/low tide info
+
+#TODO: get current weather from a station
+
+#TODO: parse weather into variables
+
+#TODO: create a clock to periodically update the high/low tides
+
+#TODO: create a clock to update the display
+
+#TODO: decide on displays 
+
+#TODO: Update display
