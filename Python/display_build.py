2a464226541b8960dc880b2f369749c2fc60155b
Mark Nyberg <bigusdeveloper@gmail.com>
2020-07-12 21:33:08 -0400

    Now using Las Palmas (Copperhead is barely working). Both files working. Mapping display_build.py to main.py and display_funcs.py to df.py. Need to refactor code for 3 x tide stations and use colors to indicate tide direction.

--- Python/display_build.py
+++ Python/display_build.py
@@ -1,11 +1,12 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 from machine import RTC, PWM, Pin, Timer
-import ntptime
 import network
 import utime
 from LCD import CharLCD
 import gc
 import urequests
+import ujson
+
 
 def wifi_connect():
     """Connect to wifi AP at beach"""
@@ -16,7 +17,7 @@ def wifi_connect():
     wlan.active(True)
     if not wlan.isconnected():
         print('connecting to network...')
-        wlan.connect('Copperhead', '9197341930')
+        wlan.connect('Las Palmas', 'pollywolly')
         while not wlan.isconnected():
             pass
     print('network config:', wlan.ifconfig())
@@ -31,7 +32,7 @@ def startup_network_disp():
     lcd.message('IP address:')
     lcd.set_line(1)
     lcd.message(ip)
-    utime.sleep(5)
+    utime.sleep(2)
 
     # display router address
     red_pin.duty(1000)
@@ -40,12 +41,28 @@ def startup_network_disp():
     lcd.message('Router address:')
     lcd.set_line(1)
     lcd.message(gateway)
-    utime.sleep(5)
+    utime.sleep(2)
     blue_pin.duty(1000)
 
     # clear display
     lcd.clear()
     gc.collect()
+
+def set_rtc():
+    """Query time API for offset and set the RTC to local time"""
+    from machine import RTC
+    rtc = RTC()
+
+    resp = urequests.get('http://worldtimeapi.org/api/timezone/America/New_York')
+
+    if resp.status_code == 200:
+        time_data = ujson.loads(resp.content)
+    else:
+        print('Response: ' + str(resp.status_code))
+
+    dt = time_data['datetime']
+    rtc.datetime((int(dt[0:4]), int(dt[5:7]), int(dt[8:10]), 0, int(dt[11:13]), int(dt[14:16]), int(dt[17:19]), 0))
+
 
 
 # yellow - red_pin.duty(0) / green_pin.duty(600)
@@ -56,5 +73,5 @@ red_pin = PWM(Pin(13), freq=1000, duty=1000)
 lcd = CharLCD(rs=4, en=5, d4=15, d5=0, d6=16, d7=2) # instantiate lcd
 
 startup_network_disp()
-
+set_rtc()
 
--- Python/display_funcs.py
+++ Python/display_funcs.py
@@ -6,12 +6,19 @@ Hits API and returns a list of tuples with 'H' or 'L' and timestamp
 Hits API and sets RTC time to New York city time
 
 
+IDEA - Do hi/lo and lo high for each location use green for rising and red for falling
+lcd.message('Beaufort tides', 2)
+lcd.set_line(1)
+lcd.message('Hi:0223  Lo:0000')
+
 """
 
 
 import urequests
 import ujson
 import utime
+from LCD import CharLCD
+from machine import Pin, PWM, RTC
 
 def get_tides():
     """Hits api for 3 days of tides, returns a list of tuples w/ hi/lo and timestamp"""
@@ -47,17 +54,60 @@ def tide_resp_to_list(resp_content):
         resp_list.append((x['type'], tmstp))
     return resp_list
 
-def set_rtc():
-    """Query time API for offset and set the RTC to local time"""
-    from machine import RTC
-    rtc = RTC()
 
-    resp = urequests.get('http://worldtimeapi.org/api/timezone/America/New_York')
+def trim_list(full_list):
+    """Review list of tuples and remove all but most recent past event"""
+    timestamp = utime.time()
+    print(timestamp)
+    first_future = 0
 
-    if resp.status_code == 200:
-        time_data = ujson.loads(resp.content)
-    else:
-        print('Response: ' + str(resp.status_code))
+    for x in range(len(full_list)):
+        first_future = x
+        if full_list[x][1] > timestamp:
+            break
 
-    dt = time_data['datetime']
-    rtc.datetime((int(dt[0:4]), int(dt[5:7]), int(dt[8:10]), 0, int(dt[11:13]), int(dt[14:16]), int(dt[17:19]), 0))
+    for x in range(first_future - 2, -1, -1):
+        full_list.pop(x)
+    return full_list
+
+def tide_display(tide_list):
+    """Will use the tide list to display the current tide status"""
+    lcd = CharLCD(rs=4, en=5, d4=15, d5=0, d6=16, d7=2)
+    green_pin = PWM(Pin(12), freq=1000, duty=1000)
+    blue_pin = PWM(Pin(14), freq=1000, duty=1000)
+    red_pin = PWM(Pin(13), freq=1000, duty=1000)
+
+    # last tide display
+    red_pin.duty(0)
+    lcd.clear
+    _, _, _, hr, min, _, _, _ = utime.localtime(tide_list[0][1])
+    hr = pad_date(hr)
+    min = pad_date(min)
+    if tide_list[0][0] == 'H':
+        lcd.message('FALLING tide', 2)
+        lcd.set_line(1)
+        lcd.message('Hi tide @ {}{}L'.format(hr, min))
+    elif tide_list[0][0] == 'L':
+        lcd.message('RISING tide', 2)
+        lcd.set_line(1)
+        lcd.message('Low tide @ {}{}L'.format(hr, min))
+    utime.sleep(5)
+    lcd.clear()
+    red_pin.duty(1000)
+
+    # next tide display
+    blue_pin.duty(0)
+    _, _, _, hr, min, _, _, _ = utime.localtime(tide_list[1][1])
+    hr = pad_date(hr)
+    min = pad_date(min)
+    if tide_list[1][0] == 'H':
+        lcd.message('RISING tide', 2)
+        lcd.set_line(1)
+        lcd.message('Hi tide @ {}{}L'.format(hr, min))
+    elif tide_list[1][0] == 'L':
+        lcd.message('FALLING tide', 2)
+        lcd.set_line(1)
+        lcd.message('Low tide @ {}{}L'.format(hr, min))
+    utime.sleep(5)
+    lcd.clear()
+    blue_pin.duty(1000)
